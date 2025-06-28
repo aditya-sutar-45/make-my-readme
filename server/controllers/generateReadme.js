785cf7a6205425dc41821aea8b3d5c2a5cd7ede6
aditya-sutar-45 <aditya1.sutar@outlook.com>
2025-06-28 10:36:42 +0000

    moved route callback to different file

---
+++ server/controllers/generateReadme.js
@@ -0,0 +1,40 @@
+import { catchAsync } from "../utils/catchAsync.js";
+import { GoogleGenAI } from "@google/genai";
+import { SYSTEM_INSTRUCTIONS } from "../config/gemini_system_instructions.js";
+
+export const generateReadme = catchAsync(async (req, res) => {
+  const { userPrompt } = req.body;
+
+  if (!userPrompt) throw new ExpressError("prompt is empty!");
+  if (userPrompt.length < 25)
+    throw new ExpressError("prompt lenght too small!");
+
+  const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API });
+  const formatedPrompt = `
+    The user has described their project as follows: 
+
+    """
+    ${userPrompt}
+    """
+
+    Based on this, generate a complete and well formated README.md file in Markdown format.
+  `;
+
+  const response = await ai.models.generateContentStream({
+    model: "gemini-2.5-flash",
+    contents: formatedPrompt,
+    config: {
+      systemInstruction: SYSTEM_INSTRUCTIONS,
+      temperature: 0.3,
+    },
+  });
+
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  res.setHeader("Cache-Control", "no-cache");
+
+  for await (const chunk of response) {
+    res.write(chunk.text);
+  }
+
+  res.end();
+});
--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
 import express, { json } from "express";
 import { ExpressError } from "./utils/ExpressError.js";
-import { catchAsync } from "./utils/catchAsync.js";
-import { GoogleGenAI } from "@google/genai";
 import "dotenv/config";
-import { SYSTEM_INSTRUCTIONS } from "./config/gemini_system_instructions.js";
+import { generateReadme } from "./controllers/generateReadme.js";
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,45 +13,7 @@ app.get("/", (req, res) => {
   res.send("hello world!!");
 });
 
-app.post(
-  "/generate-readme",
-  catchAsync(async (req, res) => {
-    const { userPrompt } = req.body;
-
-    if (!userPrompt) throw new ExpressError("prompt is empty!");
-    if (userPrompt.length < 25)
-      throw new ExpressError("prompt lenght too small!");
-
-    const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API });
-    const formatedPrompt = `
-    The user has described their project as follows: 
-
-    """
-    ${userPrompt}
-    """
-
-    Based on this, generate a complete and well formated README.md file in Markdown format.
-  `;
-
-    const response = await ai.models.generateContentStream({
-      model: "gemini-2.5-flash",
-      contents: formatedPrompt,
-      config: {
-        systemInstruction: SYSTEM_INSTRUCTIONS,
-        temperature: 0.3,
-      },
-    });
-
-    res.setHeader("Content-Type", "text/plain; charset=utf-8");
-    res.setHeader("Cache-Control", "no-cache");
-
-    for await (const chunk of response) {
-      res.write(chunk.text);
-    }
-
-    res.end();
-  })
-);
+app.post("/generate-readme", generateReadme);
 
 app.all(/(.*)/, (req, res, next) => {
   next(new ExpressError("Route does not exist!", 404));
