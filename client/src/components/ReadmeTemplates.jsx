7e93903b0dc30dbe3e3747b3a8a76967421aa4a5
Aditya Sutar <aditya1.sutar@outlook.com>
2025-07-01 19:47:35 +0530

    created basic UI (textbox and stuff)

--- client/src/components/ReadmeTemplates.jsx
+++ client/src/components/ReadmeTemplates.jsx
@@ -108,6 +108,23 @@ function ReadmeTemplate({ setMarkdown }) {
           </button>
         </div>
       </div>
+      <div className="h-[50vh] w-full">
+        <div className="h-3/12">
+          <h1 className="text-2xl text-primary text-center mt-5">
+            Generate a README.md
+          </h1>
+          <p className="m-2">
+            Describe the project you want the README.md file to be generated for
+            and you will get a response!
+          </p>
+        </div>
+        <div className="h-[50%] w-full flex justify-center items-center">
+          <textarea
+            placeholder="Primary"
+            className="textarea text-secondary resize-none h-full w-full m-3"
+          ></textarea>
+        </div>
+      </div>
     </>
   );
 }
